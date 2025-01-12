329925d580286a193da6ee59dd6a9ad2c9c0f5b9
Yasin Mooasvi <smym4244@gmail.com>
2025-01-12 12:29:09 +0330

    Add fifo empty check for Scoreboard

--- RTL/LdpcLoopWrapper/test/LdpcLoopWrapperTest/Scoreboard.svh
+++ RTL/LdpcLoopWrapper/test/LdpcLoopWrapperTest/Scoreboard.svh
@@ -24,14 +24,53 @@ class Scoreboard #(
 
     virtual task run_phase(uvm_phase phase);
         forever begin
-            ControlTransaction input_control;
-            ControlTransaction output_control;
-            DataTransaction input_data;
-            DataTransaction output_data;
-            input_control_fifo.get(input_control);
-            output_control_fifo.get(output_control);
-            input_data_fifo.get(input_data);
-            output_data_fifo.get(output_data);
+            fork
+                check_control();
+                check_data();
+            join
         end
     endtask
+
+    virtual function void check_phase(uvm_phase phase);
+        if (input_control_fifo.used() != 0)
+            `uvm_error("SCOREBOARD", $sformatf(
+                "input_control_fifo is not empty! remained transactions: %0d", input_control_fifo.used()))
+        if (input_data_fifo.used() != 0)
+            `uvm_error("SCOREBOARD", $sformatf(
+                "input_data_fifo is not empty! remained transactions: %0d", input_data_fifo.used()))
+        if (output_control_fifo.used() != 0)
+            `uvm_error("SCOREBOARD", $sformatf(
+                "output_control_fifo is not empty! remained transactions: %0d", output_control_fifo.used()))
+        if (output_data_fifo.used() != 0)
+            `uvm_error("SCOREBOARD", $sformatf(
+                "output_data_fifo is not empty! remained transactions: %0d", output_data_fifo.used()))
+    endfunction
+
+    local task check_control();
+        ControlTransaction input_control;
+        ControlTransaction output_control;
+        input_control_fifo.get(input_control);
+        output_control_fifo.get(output_control);
+        check_control_matching(input_control, output_control);
+    endtask
+
+    local task check_data();
+        DataTransaction input_data;
+        DataTransaction output_data;
+        input_data_fifo.get(input_data);
+        output_data_fifo.get(output_data);
+        check_data_matching(input_data, output_data);
+    endtask
+
+    local function void check_control_matching(ControlTransaction in, ControlTransaction out);
+        if (!in.compare(out)) begin
+            `uvm_error("SCOREBOARD", $sformatf("transactions don't match! \nin: %s\nout: %s\n", in.sprint(), out.sprint()))
+        end
+    endfunction
+
+    local function void check_data_matching(DataTransaction in, DataTransaction out);
+        if (!in.compare(out)) begin
+            `uvm_error("SCOREBOARD", $sformatf("transactions don't match! \nin: %s\nout: %s\n", in.sprint(), out.sprint()))
+        end
+    endfunction
 endclass
