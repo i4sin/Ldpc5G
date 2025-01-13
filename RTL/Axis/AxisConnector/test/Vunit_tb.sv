fae44cdd878a287fe566653d197211505a6d8370
Yasin Mooasvi <smym4244@gmail.com>
2025-01-13 09:08:27 +0330

    Remove unnecessary ConfigDb sets from Vunit_tbs

--- RTL/Axis/AxisConnector/test/Vunit_tb.sv
+++ RTL/Axis/AxisConnector/test/Vunit_tb.sv
@@ -50,7 +50,6 @@ module AxisConnectorTest();
     `TEST_SUITE_FROM_PARAMETER(runner_config) begin
         `TEST_CASE_SETUP begin
             uvm_top.finish_on_completion = 0;
-            ConfigDb#(int)::set(null, "uvm_test_top.master_seq", "packets_count", PACKETS_COUNT);
             ConfigDb#(longint)::set(null, "uvm_test_top.watchdog", "watchdog_clocks", WATCHDOG_CLOCKS);
             ConfigDb#(virtual ClockIf)::set(null, "uvm_test_top.watchdog", "clock_vif", clock_if);
             ConfigDb#(virtual ResetIf)::set(null, "uvm_test_top.env", "reset_vif", reset_if);
