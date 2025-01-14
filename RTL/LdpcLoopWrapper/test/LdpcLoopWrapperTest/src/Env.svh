fc4a73e6a6211f6201552c0b55ebe85ff1c2e7db
Yasin Mooasvi <smym4244@gmail.com>
2025-01-14 15:05:42 +0330

    Add ControlStatusMap to Scoreboard

--- RTL/LdpcLoopWrapper/test/LdpcLoopWrapperTest/src/Env.svh
+++ RTL/LdpcLoopWrapper/test/LdpcLoopWrapperTest/src/Env.svh
@@ -56,9 +56,9 @@ class Env #(
         data_master_agent.set_vif(s_axis_data_vif);
         decoder_status_slave_agent.set_vif(m_axis_decoder_status_vif);
         data_slave_agent.set_vif(m_axis_data_vif);
-        encoder_control_master_agent.port.connect(scoreboard.input_control_fifo.analysis_export);
+        encoder_control_master_agent.port.connect(scoreboard.input_control_export);
         data_master_agent.port.connect(scoreboard.input_data_fifo.analysis_export);
-        decoder_status_slave_agent.port.connect(scoreboard.output_status_fifo.analysis_export);
+        decoder_status_slave_agent.port.connect(scoreboard.output_status_export);
         data_slave_agent.port.connect(scoreboard.output_data_fifo.analysis_export);
     endfunction
 
--- RTL/LdpcLoopWrapper/test/LdpcLoopWrapperTest/src/Scoreboard.svh
+++ RTL/LdpcLoopWrapper/test/LdpcLoopWrapperTest/src/Scoreboard.svh
@@ -5,21 +5,40 @@ class Scoreboard #(
     `uvm_component_param_utils(Scoreboard#(CONTROL_STATUS_WIDTH, DATA_WIDTH))
 
     typedef AxisTransaction#(CONTROL_STATUS_WIDTH) AxisControlStatusTransaction;
+    typedef ControlStatusTransaction#(CONTROL_STATUS_WIDTH) ControlStatusTransaction;
     typedef AxisTransaction#(DATA_WIDTH) DataTransaction;
-    typedef uvm_tlm_analysis_fifo #(AxisControlStatusTransaction) ControlStatusAnalysisFifo;
+    typedef uvm_analysis_export #(AxisControlStatusTransaction) AxisControlStatusExport;
+    typedef ControlStatusMap #(CONTROL_STATUS_WIDTH) ControlStatusMap;
+    typedef uvm_tlm_analysis_fifo #(ControlStatusTransaction) ControlStatusAnalysisFifo;
     typedef uvm_tlm_analysis_fifo #(DataTransaction) DataAnalysisFifo;
 
-    ControlStatusAnalysisFifo input_control_fifo;
-    ControlStatusAnalysisFifo output_status_fifo;
+    AxisControlStatusExport input_control_export;
+    AxisControlStatusExport output_status_export;
     DataAnalysisFifo input_data_fifo;
     DataAnalysisFifo output_data_fifo;
 
+    local ControlStatusMap input_control_map;
+    local ControlStatusMap output_status_map;
+    local ControlStatusAnalysisFifo input_control_fifo;
+    local ControlStatusAnalysisFifo output_status_fifo;
+
     function new(string name, uvm_component parent);
         super.new(name, parent);
+        input_control_export = new("input_control_export", this);
+        output_status_export = new("output_status_export", this);
+        input_control_map = new("input_control_map", this);
+        output_status_map = new("output_status_map", this);
         input_control_fifo = new("input_control_fifo", this);
         output_status_fifo = new("output_status_fifo", this);
         input_data_fifo = new("input_data_fifo", this);
         output_data_fifo = new("output_data_fifo", this);
+    endfunction
+
+    virtual function void connect_phase(uvm_phase phase);
+        input_control_export.connect(input_control_map.imp);
+        output_status_export.connect(output_status_map.imp);
+        input_control_map.port.connect(input_control_fifo.analysis_export);
+        output_status_map.port.connect(output_status_fifo.analysis_export);
     endfunction
 
     virtual task run_phase(uvm_phase phase);
@@ -47,8 +66,8 @@ class Scoreboard #(
     endfunction
 
     local task check_control_status();
-        AxisControlStatusTransaction input_control;
-        AxisControlStatusTransaction output_status;
+        ControlStatusTransaction input_control;
+        ControlStatusTransaction output_status;
         input_control_fifo.get(input_control);
         output_status_fifo.get(output_status);
         check_matching(input_control, output_status);
